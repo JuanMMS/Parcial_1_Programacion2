f0a892494addf93afade864f33c29543f26fd703
JuanMMS <juanm.mosqueras@uqvirtual.edu.co>
2026-09-24 21:02:16 -0500

    Update Factory

--- App_SmartGym/src/main/java/uniquindio/app_smartgym/model/Factory/FactoryPlanEntrenamiento.java
+++ App_SmartGym/src/main/java/uniquindio/app_smartgym/model/Factory/FactoryPlanEntrenamiento.java
@@ -1,8 +1,15 @@
-package uniquindio.app_smartgym.Model.Factory;
+package uniquindio.app_smartgym.model.Factory;
 
 public abstract class FactoryPlanEntrenamiento {
 
+
+    /**
+     * Metodo factory de plan entrenamiento
+     * @param codigo
+     * @return Plan Entrenamiento
+     */
     public abstract uniquindio.app_smartgym.Model.PlanEntrenamiento crearPlanEntrenamiento(String codigo);
+
 
 
 }
